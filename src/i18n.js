346ccc0f7a34c57e5f48de211fe2350d8e5a1fac
Marco Solari <marcosolari@gmail.com>
2021-05-19 16:06:49 +0200

    Testing i18n

--- src/i18n.js
+++ src/i18n.js
@@ -1,10 +1,11 @@
 import i18n from 'i18next';
+import languageDetector from 'i18next-browser-languagedetector';
 import { reactI18nextModule } from 'react-i18next';
 
 import translationEN from './locales/en/translation.json';
 import translationIT from './locales/it/translation.json';
 
-const lng = 'it'; // TODO: => defaultLanguage ?
+const fallbackLng = 'en'; // fallback language (detected language is not available)
 
 // the translations
 const resources = {
@@ -17,10 +18,11 @@ const resources = {
 };
 
 i18n
+  .use(languageDetector) // browser's language detector
   .use(reactI18nextModule) // passes i18n down to react-i18next
   .init({
     resources,
-    lng,
+    fallbackLng, // use en if detected language is not available
     keySeparator: false, // we do not use keys in form messages.welcome
     interpolation: {
       escapeValue: false, // react already safes from xss
