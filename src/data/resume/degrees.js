e7c34d59f388242a594556186b77c1df7adc8055
Marco Solari <marcosolari@gmail.com>
2021-05-24 12:37:35 +0200

    Writing contents :-)

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,15 +1,9 @@
 const degrees = [
   {
-    school: 'Stanford University',
-    degree: 'M.S. Computational and Mathematical Engineering',
-    link: 'https://stanford.edu',
-    year: 2016,
-  },
-  {
-    school: 'University at Buffalo',
-    degree: 'B.S. Electrical Engineering, Computer Engineering',
-    link: 'https://buffalo.edu',
-    year: 2012,
+    school: 'Liceo A. Pacinotti a La Spezia',
+    degree: 'Diploma Scientifico',
+    link: 'https://www.liceopacinotti.edu.it',
+    year: 1979,
   },
 ];
 
